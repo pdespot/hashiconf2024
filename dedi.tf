8c278336018c207ec472e70cfc99f7e7680699a3
pdespot <60762694+pdespot@users.noreply.github.com>
2024-10-07 17:03:20 -0400

    Update dedi.tf

--- dedi.tf
+++ dedi.tf
@@ -13,13 +13,10 @@ provider "linode" {
 
 resource "linode_instance" "terraform-web" {
         image = "linode/ubuntu24.04"
-        label = "Example created by Terraform"
-        group = "HashiConf"
+        label = "Demo VM"
+        group = "Hashiconf 2024"
         region = "us-east"
         type = "g6-standard-1"
-        authorized_keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCFRxTCwPmJOguXvWD4+bYQfrnzN4RDE1wel1v69u1tlR1MWLpi4nMyaNduT8
-GGUEzhEmTuNPPMtpq+3/3PGf/vNbNGoW+BtrafjSR/nT++WIaLW+gRJT3J1wcM2cD0tJh/4Rjkg7siJSUNm7B01xFFGmU71VCG0irufKUXIpnt6WuOxLceVv8JizwZU
-6W0b0woKaLIgY0kaVEuVk955LVAkwJVd2fPxXio6aSsAQ5gcGKwEldZ60DkFlGGmPeGhFBH+QEPJIXnDRtgk845wyJScdbGwlB+CfNyl1+0I4wwsD44PBLgRiJGIDp5
-Nrk3Sf/z0cqjalRFPsknGUXaAEkr pdespot-linode-20240925" ]
+        authorized_keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCFRxTCwPmJOguXvWD4+bYQfrnzN4RDE1wel1v69u1tlR1MWLpi4nMyaNduT8GGUEzhEmTuNPPMtpq+3/3PGf/vNbNGoW+BtrafjSR/nT++WIaLW+gRJT3J1wcM2cD0tJh/4Rjkg7siJSUNm7B01xFFGmU71VCG0irufKUXIpnt6WuOxLceVv8JizwZU6W0b0woKaLIgY0kaVEuVk955LVAkwJVd2fPxXio6aSsAQ5gcGKwEldZ60DkFlGGmPeGhFBH+QEPJIXnDRtgk845wyJScdbGwlB+CfNyl1+0I4wwsD44PBLgRiJGIDp5Nrk3Sf/z0cqjalRFPsknGUXaAEkr pdespot-linode-20240925" ]
         root_pass = "AkanodeDem0!"
 }
