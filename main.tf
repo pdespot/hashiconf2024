f56bf89d3e91c5cb0c9d6c150ccd13302aef5427
pdespot <60762694+pdespot@users.noreply.github.com>
2024-10-07 17:43:40 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+variable "regions" {
+  description = "List of regions to create Linodes in"
+  type        = list(string)
+}
+
+variable "userid" {
+  description = "User ID or identifier to be used in labels and tags"
+  type        = string
+}
+
+terraform {
+  required_providers {
+    linode = {
+      source = "linode/linode"
+      version = "2.5.2"
+    }
+  }
+}
+
+provider "linode" {
+  token = "0ff1d17b3c336735f07cb9f4aaa4ce44adb8f7125b691c0aa5ea120a9bdbc0ac"
+}
+
+resource "linode_instance" "terraform-web" {
+        count = length(var.regions)
+        label = "HashiConf-${element(var.regions, count.index)}"
+        region = element(var.regions, count.index)
+        image = "linode/ubuntu24.04"
+        group = "HashiConf2024"
+        type = "g6-standard-1"
+        tags = toset([var.userid])
+        authorized_keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCFRxTCwPmJOguXvWD4+bYQfrnzN4RDE1wel1v69u1tlR1MWLpi4nMyaNduT8GGUEzhEmTuNPPMtpq+3/3PGf/vNbNGoW+BtrafjSR/nT++WIaLW+gRJT3J1wcM2cD0tJh/4Rjkg7siJSUNm7B01xFFGmU71VCG0irufKUXIpnt6WuOxLceVv8JizwZU6W0b0woKaLIgY0kaVEuVk955LVAkwJVd2fPxXio6aSsAQ5gcGKwEldZ60DkFlGGmPeGhFBH+QEPJIXnDRtgk845wyJScdbGwlB+CfNyl1+0I4wwsD44PBLgRiJGIDp5Nrk3Sf/z0cqjalRFPsknGUXaAEkr pdespot-linode-20240925" ]
+        root_pass = "AkanodeDem0!"
+}
